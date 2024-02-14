d6955c98980dbbf6eb9e62dd8046112d51e174ac
jiwanchoi <wldhks303@naver.com>
2024-02-14 21:28:36 +0900

    Create landing, login, errorPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import TextInput from '../../components/Common/TextInput/TextInput';
-import { LoginTitle, LoginWrapper } from './LoginPage.styled';
-import GenderButton from '../../components/Common/GenderButton/GenderButton';
+import { LoginTitle, LoginWrapper, ButtonWrapper } from './LoginPage.styled';
+import Button from '../../components/Common/Button/Button';
 
 function LoginPage() {
   return (
     <LoginWrapper>
       <LoginTitle>로그인</LoginTitle>
-      <TextInput
-        name="이메일"
-        placeholder="이메일 주소"
-        inputType="email"
-        form
-        onChange={() => {}}
-      />
-      <TextInput
-        name="비밀번호"
-        placeholder="영문, 숫자 포함 10자 이내"
-        inputType="password"
-        form
-        onChange={() => {}}
-      />
-      <GenderButton name="성별" />
+      <TextInput name="" placeholder="이메일" inputType="email" form onChange={() => {}} />
+      <TextInput name="" placeholder="비밀번호" inputType="password" form onChange={() => {}} />
+      <ButtonWrapper>
+        <Button type="enable" text="로그인" to="" onClick={() => {}} />
+      </ButtonWrapper>
     </LoginWrapper>
   );
 }
