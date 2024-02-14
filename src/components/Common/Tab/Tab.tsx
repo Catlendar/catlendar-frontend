1d47293fbc702089ead87aaa9a886ecb78030275
swJaNG12 <cbdjej38483@gmail.com>
2024-02-14 20:58:45 +0900

    할 일 탭, 운세 탭 분기처리

--- src/components/Common/Tab/Tab.tsx
+++ src/components/Common/Tab/Tab.tsx
@@ -2,13 +2,14 @@
 
 import { useState } from 'react';
 import { TabBox, TabButton, TabWrapper } from './Tab.styled';
-import { GetTabStyle, TabProps } from './TabTypes';
+import { GetTabStyle, TabProps, TabMenuTypeTodo, TabMenuTypeFortune } from './TabTypes';
 
 export default function Tab({ tabData }: TabProps) {
   const [selectedTab, setSelectedTab] = useState<number | undefined>(tabData[0]?.id);
 
   const TabType = tabData[0]?.__type;
 
+  // 현재 탭이 할 일 탭인지, 운세 탭인지 구분해서 스타일 반환
   const getTabStyle: GetTabStyle = (type) => {
     switch (type) {
       case 'todo':
@@ -31,11 +32,11 @@ export default function Tab({ tabData }: TabProps) {
         };
     }
   };
-  // interface GetTodo {
-  // 	(userId: number):
-  // }
+  const tabStyle = getTabStyle(TabType);
 
+  // 오늘 할 일 api 호출
   const getTodo = async (userId: string) => {
+    console.log('getTodo!');
     try {
       const response = await fetch('http://54.66.123.168:8080/calendar/getToday', {
         method: 'POST',
@@ -57,8 +58,34 @@ export default function Tab({ tabData }: TabProps) {
       console.log(error);
     }
   };
+  // 즐겨찾기 api 호출
+  const getFavorite = async (userId: string) => {
+    console.log('getFavorit!');
+  };
 
-  const tabStyle = getTabStyle(TabType);
+  // 운세 api 호출
+  const handleFortuneTabClick = (tab: TabMenuTypeTodo | TabMenuTypeFortune) => {
+    console.log('운세 찾기 실행');
+    console.log(tab);
+  };
+
+  // 클릭한 탭이 할 일 탭인지, 운세 탭인지 구분
+  // 흐름 : todo => getTodo(api, 오늘 할 일 탭) : getFavorite(api, 즐겨찾기 탭)
+  // 흐름 : fortune => handleFortuneTabClick(운세 탭 이라면)
+  const handleTabClick = (tab: TabMenuTypeTodo | TabMenuTypeFortune) => {
+    setSelectedTab(tab.id);
+    console.log(tab);
+    if (tab.__type === 'todo') {
+      if (tab.tabName === '오늘') {
+        getTodo('5');
+      } else {
+        getFavorite('5');
+      }
+    } else {
+      handleFortuneTabClick(tab);
+    }
+  };
+
   return (
     <TabWrapper tabStyle={tabStyle}>
       <TabBox tabStyle={tabStyle}>
@@ -66,8 +93,7 @@ export default function Tab({ tabData }: TabProps) {
           <TabButton
             key={tab.id}
             onClick={() => {
-              setSelectedTab(tab.id);
-              getTodo('5');
+              handleTabClick(tab);
             }}
             isSelect={selectedTab === tab.id}
           >
