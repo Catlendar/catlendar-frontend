ba3cae187513a3c873657b8218bcdd9562cffb1a
jiwanchoi <wldhks303@naver.com>
2024-02-19 15:24:39 +0900

    충돌 수정

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import TodoBox from '../../components/Common/TodoBox/TodoBox';
+import MainCard from '../../components/MainCard/MainCard';
 
 export default function HomePage() {
   return (
     <div style={{ boxShadow: 'rgba(255, 255, 0, 0.35) 0px 0px 5px 20px', width: '390px' }}>
       {/* <div style={{ height: '300px', backgroundColor: 'aqua' }}>ss</div> */}
+      <MainCard />
       <TodoBox />
     </div>
   );
