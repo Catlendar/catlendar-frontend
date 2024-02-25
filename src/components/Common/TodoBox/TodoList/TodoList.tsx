0d0971cb9855bb1e31e66d4cceba35c329ae9274
jiwanchoi <wldhks303@naver.com>
2024-02-25 23:04:21 +0900

    homepage 할일 리스트 확장, 할일 항목 내용 축약기능추가

--- src/components/Common/TodoBox/TodoList/TodoList.tsx
+++ src/components/Common/TodoBox/TodoList/TodoList.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { TodoListAtom } from '../../../../atom/TodoListAtom';
-import { TodoListUl, TodoListWrapper } from './TodoList.styled';
+import { TodoListUl, TodoListWrapper, MoreBtn } from './TodoList.styled';
 import TodoListItem from './TodoListItem/TodoListItem';
+import Icon from '../../../../assets/icons/icon-more-meatball.svg';
 
 export default function TodoList() {
   const todoListAtom = useRecoilValue(TodoListAtom);
+  const [showAll, setShowAll] = useState(false);
   console.log(todoListAtom);
 
+  const visibleItems = showAll ? todoListAtom : todoListAtom.slice(0, 8);
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
   return (
     <TodoListWrapper>
       <TodoListUl>
-        {todoListAtom.length === 0 ? (
+        {visibleItems.length === 0 ? (
           <p>오늘의 할일을 추가해보세요.</p>
         ) : (
-          todoListAtom.map((todo) => <TodoListItem todo={todo} key={todo.calendarId} />)
+          visibleItems.map((todo) => <TodoListItem todo={todo} key={todo.calendarId} />)
+        )}
+        {todoListAtom.length > 8 && (
+          <MoreBtn className="moreBtn" onClick={toggleShowAll}>
+            {showAll ? null : <img src={Icon} alt="더보기" />}
+          </MoreBtn>
         )}
       </TodoListUl>
     </TodoListWrapper>
